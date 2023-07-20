c1fe6a99972cd3fba061e6ed8ae2d23fb98abd35
deMGoncalves <cleber.engineer@hotmail.com>
2023-07-20 15:48:50 -0300

    refactor: organizando a estrutura, este projeto pode ser a base para outros components

--- src/components/carousel/storage.js
+++ src/components/carousel/storage.js
@@ -5,8 +5,11 @@ import result from '@standard/result'
 
 const storage = middleware(async function (carousel) {
   const url = '/b/explore-aulas'
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+  }
   const { category, type } = carousel
-  const { data, error } = await http.post(url).body({ category, type }).json()
+  const { data, error } = await http.post(url).headers(headers).body({ category, type }).json()
   error
     ? carousel[result.Error]?.(mock)
     : carousel[result.Ok]?.(mock)
