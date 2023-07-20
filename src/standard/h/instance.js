bc75959f942a6d03a835364ee7a202b23645a1bc
deMGoncalves <cleber.engineer@hotmail.com>
2023-07-20 09:09:51 -0300

    docs(h): documentando o codigo do modulo h

--- src/standard/h/instance.js
+++ src/standard/h/instance.js
@@ -1,6 +1,16 @@
 import paint from './paint'
 
+/**
+ * Utility class for mapping and processing instances of elements or components.
+ */
 class Instance {
+  /**
+   * Maps the given array of children elements or components, returning their root AST or the original child if the root AST is not available.
+   *
+   * @static
+   * @param {Array<Element|Component>} children - The array of children elements or components to map.
+   * @returns {Array<Element|Component>} The mapped array of elements or components.
+   */
   static mapper (children) {
     return children.map((child) => child[paint.rootAST]?.() ?? child)
   }
