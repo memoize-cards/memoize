c7352d92d72a35425f31a7f8dbed462540285de0
deMGoncalves <cleber.engineer@hotmail.com>
2023-07-20 08:52:15 -0300

    docs(h): documentando o codigo do modulo h

--- src/standard/h/element.js
+++ src/standard/h/element.js
@@ -17,6 +17,24 @@ import willMount from './willMount'
 import willUpdate from './willUpdate'
 import willUnmount from './willUnmount'
 
+/**
+ * Class representing a DOM element with reactive behavior.
+ *
+ * @class
+ * @decorator revoke
+ * @param {string} nodeName - The name of the DOM node to be created.
+ * @param {Object} attrs - The attributes to be set on the element.
+ * @param {Array} children - The children elements to be appended to the element.
+ * @property {Attributes} attributes - The attributes object.
+ * @property {Children} children - The children object.
+ * @property {ClassName} className - The class name object.
+ * @property {Events} events - The events object.
+ * @property {string} is - The custom element name.
+ * @property {string} key - The key attribute value.
+ * @property {string} nodeName - The name of the DOM node.
+ * @property {string} slot - The slot attribute value.
+ * @property {HTMLElement} __node__ - The actual DOM node.
+ */
 @revoke
 class Element {
   #attributes
@@ -29,42 +47,96 @@ class Element {
   #nodeName
   #slot
 
+  /**
+   * Get the attributes object.
+   *
+   * @type {Attributes}
+   */
   get attributes () {
     return this.#attributes
   }
 
+  /**
+   * Get the children object.
+   *
+   * @type {Children}
+   */
   get children () {
     return this.#children
   }
 
+  /**
+   * Get the class name object.
+   *
+   * @type {ClassName}
+   */
   get className () {
     return this.#className
   }
 
+  /**
+   * Get the events object.
+   *
+   * @type {Events}
+   */
   get events () {
     return this.#events
   }
 
+  /**
+   * Get the custom element name.
+   *
+   * @type {string}
+   */
   get is () {
     return this.#is.value
   }
 
+  /**
+   * Get the key attribute value.
+   *
+   * @type {string}
+   */
   get key () {
     return this.#key.value
   }
 
+  /**
+   * Get the name of the DOM node.
+   *
+   * @type {string}
+   */
   get nodeName () {
     return this.#nodeName
   }
 
+  /**
+   * Get the slot attribute value.
+   *
+   * @type {string}
+   */
   get slot () {
     return this.#slot.value
   }
 
+  /**
+   * Get the actual DOM node.
+   *
+   * @type {HTMLElement}
+   * @private
+   */
   get __node__ () {
     return this.#node
   }
 
+  /**
+   * Create a new Element instance.
+   *
+   * @constructor
+   * @param {string} nodeName - The name of the DOM node to be created.
+   * @param {Object} attrs - The attributes to be set on the element.
+   * @param {Array} children - The children elements to be appended to the element.
+   */
   constructor (nodeName, attrs, children) {
     this.#nodeName = nodeName
     this.#attributes = Attributes.create(attrs, this)
@@ -76,29 +148,58 @@ class Element {
     this.#slot = Slot.create(attrs)
   }
 
+  /**
+   * Add an event listener to the element.
+   *
+   * @param {Array} event - An array containing the event name and the event handler function.
+   * @returns {Element} The current Element instance.
+   */
   addEventListener (event) {
     Reflect.set(this.#node, ...event)
     return this
   }
 
+  /**
+   * Append a child element after a specified child element.
+   *
+   * @param {Element} child - The child element to insert after.
+   * @returns {Element} The current Element instance.
+   */
   after (child) {
     const node = child[render.flow]()
     this.#node.after(node)
     return this
   }
 
+  /**
+   * Append an array of child elements to the element.
+   *
+   * @param {Array} childList - An array containing the child elements to be appended.
+   * @returns {Element} The current Element instance.
+   */
   append (childList) {
     const nodeList = childList.map((child) => child[render.flow]())
     this.#node.append(...nodeList)
     return this
   }
 
+  /**
+   * Append a child element to the element.
+   *
+   * @param {Element} child - The child element to be appended.
+   * @returns {Element} The current Element instance.
+   */
   appendChild (child) {
     const node = child[render.flow]()
     this.#node.appendChild(node)
     return this
   }
 
+  /**
+   * Remove the element from the DOM.
+   *
+   * @returns {Element} The current Element instance.
+   */
   @didUnmount
   @willUnmount
   remove () {
@@ -106,27 +207,58 @@ class Element {
     return this
   }
 
+  /**
+   * Remove an attribute from the element.
+   *
+   * @param {Object} attr - The attribute object to be removed.
+   * @returns {Element} The current Element instance.
+   */
   removeAttribute (attr) {
     this.#node.removeAttribute(attr?.key)
     return this
   }
 
+  /**
+   * Remove an event listener from the element.
+   *
+   * @param {Object} event - The event object to be removed.
+   * @returns {Element} The current Element instance.
+   */
   removeEventListener (event) {
     delete this.#node[event.name]
     return this
   }
 
+  /**
+   * Replace a child element with another element.
+   *
+   * @param {Element} child - The child element to be replaced.
+   * @param {Element} nChild - The new child element to replace the old one.
+   * @returns {Element} The current Element instance.
+   */
   replace (child, nChild) {
     child.after(nChild)
     child.remove()
     return this
   }
 
+  /**
+   * Set an attribute on the element.
+   *
+   * @param {Object} attr - The attribute object to be set.
+   * @returns {Element} The current Element instance.
+   */
   setAttribute (attr) {
     this.#node.setAttribute(...attr)
     return this
   }
 
+  /**
+   * Check if the current Element is different from the new Element during a reflow.
+   *
+   * @param {Element} nElement - The new Element to compare.
+   * @returns {boolean} Whether the Elements are different or not.
+   */
   [reflow.different] (nElement) {
     return (
       (this.nodeName !== nElement.nodeName) ||
@@ -134,6 +266,12 @@ class Element {
     )
   }
 
+  /**
+   * Check if the current Element is the same as the new Element during a reflow.
+   *
+   * @param {Element} nElement - The new Element to compare.
+   * @returns {boolean} Whether the Elements are the same or not.
+   */
   [reflow.same] (nElement) {
     return (
       (this.key !== undefined && this.key === nElement.key) ||
@@ -141,6 +279,11 @@ class Element {
     )
   }
 
+  /**
+   * Create the actual DOM node and flow the attributes, class name, events, and children.
+   *
+   * @returns {HTMLElement} The actual DOM node.
+   */
   @didMount
   @willMount
   [render.flow] () {
@@ -152,6 +295,12 @@ class Element {
     return this.#node
   }
 
+  /**
+   * Repaint the element during an update.
+   *
+   * @param {Element} nElement - The new Element to repaint with.
+   * @returns {Element} The current Element instance.
+   */
   @didUpdate
   @willUpdate
   [repaint.reflow] (nElement) {
@@ -162,12 +311,28 @@ class Element {
     return this
   }
 
+  /**
+   * Create a new Element instance.
+   *
+   * @static
+   * @param {string} nodeName - The name of the DOM node to be created.
+   * @param {Object} attrs - The attributes to be set on the element.
+   * @param {Array} children - The children elements to be appended to the element.
+   * @returns {Element} A new Element instance.
+   */
   static create (nodeName, attrs, children) {
     attrs = Object.entries(attrs)
     children = children.flat(Infinity)
     return new Element(nodeName, attrs, children)
   }
 
+  /**
+   * Check if the given nodeName represents an actual DOM element (string type).
+   *
+   * @static
+   * @param {*} nodeName - The name of the DOM node to check.
+   * @returns {boolean} Whether the nodeName represents a DOM element or not.
+   */
   static is (nodeName) {
     return typeof nodeName === 'string'
   }
