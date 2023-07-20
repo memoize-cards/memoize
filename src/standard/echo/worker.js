c97e46246d2e10fd48c4fd1cafce82b6d372c4cd
deMGoncalves <cleber.engineer@hotmail.com>
2023-07-20 10:36:41 -0300

    docs(echo): documentacao e codigo e uso

--- src/standard/echo/worker.js
+++ src/standard/echo/worker.js
@@ -1,3 +1,10 @@
 import blob from './blob'
 
+/**
+ * Creates a new Worker instance using the provided blob.
+ * The blob is an object URL representing the worker's script.
+ *
+ * @param {string} objectURL - The object URL of the worker's script blob.
+ * @returns {Worker} The new Worker instance.
+ */
 export default new Worker(window.URL.createObjectURL(blob))
