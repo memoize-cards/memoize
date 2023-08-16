16a89a833c54028ac12e9045589a6fc47684bb0f
deMGoncalves <cleber.engineer@hotmail.com>
2023-08-16 16:17:03 -0300

    refactor(termsofuse): padronizando a url

--- src/site/termsOfUse/index.jsx
+++ src/site/termsOfUse/index.jsx
@@ -1,7 +1,7 @@
 import h, { render } from '@standard/h'
 import router from '@standard/router'
 
-router('/termsOfUse', async function termsOfUse () {
+router('/terms-of-use', async function termsOfUse () {
   const { default: TermsOfUse } = await import('./termsOfUse' /* webpackChunkName: "termsOfUse" */)
   render(document.body, <TermsOfUse />)
 })
