b4d3ef41683dfbb5e65b248a4b570d17fdfe155b
deMGoncalves <cleber.engineer@hotmail.com>
2023-08-18 17:06:00 -0300

    refactor: troca do logo da marca

--- src/site/termsOfUse/component.jsx
+++ src/site/termsOfUse/component.jsx
@@ -1,11 +1,15 @@
 import h from '@standard/h'
 import Header from './header'
 import Main from './main'
+import text from '@elements/text'
 
 function component () {
   return (
     <Main>
       <Header />
+      <article>
+        <text.H1></text.H1>
+      </article>
     </Main>
   )
 }
