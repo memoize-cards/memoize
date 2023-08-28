60a4a998a4887a68cbc0562c5cf1b61763473714
deMGoncalves <cleber.engineer@hotmail.com>
2023-08-28 18:25:29 -0300

    style(createDeck): padronizacao do codigo

--- src/app/createDeck/storage.js
+++ src/app/createDeck/storage.js
@@ -7,9 +7,9 @@ const { onCreated, onError } = magic
 
 const create = interceptor(function (args, next) {
   setImmediate(async () => {
-    const { id: user_id } = cookie
+    const { id } = cookie
     const { name, description } = this
-    const { data, error } = await supabase.from('deck').insert([{ name, description, user_id }]).select()
+    const { data, error } = await supabase.from('deck').insert([{ name, description, user_id: id }]).select()
     error
       ? this[onError]?.(error)
       : this[onCreated]?.(data)
