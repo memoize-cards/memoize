9d4baf36736fef6f10c91637acb911f59ac15442
deMGoncalves <cleber.engineer@hotmail.com>
2023-08-29 17:14:08 -0300

    refactor(deck): passando id para o urlFor

--- src/app/deck/edit.js
+++ src/app/deck/edit.js
@@ -1,11 +1,11 @@
-import { urlFor } from '@standard/router'
+import { params, urlFor } from '@standard/router'
 import h from '@standard/h'
 import text from '@elements/text'
 import translate from './translate'
 
 function component () {
   return (
-    <text.A href={urlFor('editDeck')} primary xxs medium>{translate.edit}</text.A>
+    <text.A href={urlFor('editDeck', { id: params.id })} primary xxs medium>{translate.edit}</text.A>
   )
 }
 
