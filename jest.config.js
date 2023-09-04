b64b73d23a7d54dac86759677e6ec7162dce05ba
deMGoncalves <cleber.engineer@hotmail.com>
2023-09-04 15:10:40 -0300

    ci(codeclimate): configuracao dos badges e coverage

--- jest.config.js
+++ jest.config.js
@@ -20,30 +20,38 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    '<rootDir>/**/*.{js,jsx}'
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/'
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: [
-    'json',
-    'text',
-    'lcov',
-    'clover',
-    'text-summary'
+  // "json",
+  // "text",
+  // "lcov",
+  // "clover"
   ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
+  coverageThreshold: {
+    global: {
+      branches: 10,
+      functions: 10,
+      lines: 10,
+      statements: 10
+    }
+  },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
