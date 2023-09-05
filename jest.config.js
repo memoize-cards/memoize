75532cdbf2280ffacb5f98536c200fa69f2b684d
deMGoncalves <cleber.engineer@hotmail.com>
2023-09-05 19:34:03 -0300

    ci: configuracao do codacy coverage report

--- jest.config.js
+++ jest.config.js
@@ -25,7 +25,7 @@ module.exports = {
   ],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: '../',
+  coverageDirectory: '../coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
