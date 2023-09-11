de662f0d9ef7aae2203d6537f0782c4b2d064422
deMGoncalves <cleber.engineer@hotmail.com>
2023-09-11 16:50:25 -0300

    chore(markdown): agora o h nao eh async

--- src/pixel/markdown/markdown.jsx
+++ src/pixel/markdown/markdown.jsx
@@ -3,24 +3,12 @@ import showdown from '@artifact/showdown'
 
 @define('m-markdown')
 class Markdown extends HTMLElement {
-  #observer = new MutationObserver(() => this.#render())
-
   constructor () {
     super()
     this.attachShadow({ mode: 'open' })
   }
 
   connectedCallback () {
-    this.#observer.observe(this, { childList: true })
-    return this
-  }
-
-  disconnectedCallback () {
-    this.#observer.disconnect()
-    return this
-  }
-
-  #render () {
     this.shadowRoot.innerHTML = showdown.makeHtml(this.innerHTML)
     return this
   }
