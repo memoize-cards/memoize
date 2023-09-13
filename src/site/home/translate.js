9633940a306753faa92461fefa392a8d03552917
deMGoncalves <cleber.engineer@hotmail.com>
2023-09-13 20:00:37 -0300

    chore(home): traducao da home

--- src/site/home/translate.js
+++ src/site/home/translate.js
@@ -4,28 +4,28 @@ import lang from '@standard/lang'
 const dictionary = {
   description: {
     [lang.EN]: 'An advanced flashcard application designed to optimize the learning and memorization process.',
-    [lang.ES]: 'An advanced flashcard application designed to optimize the learning and memorization process.',
-    [lang.PT]: 'An advanced flashcard application designed to optimize the learning and memorization process.'
+    [lang.ES]: 'Una aplicación avanzada de tarjetas de memoria diseñada para optimizar el proceso de aprendizaje y memorización.',
+    [lang.PT]: 'Um aplicativo avançado de flashcard projetado para otimizar o processo de aprendizado e memorização.'
   },
   getStarted: {
     [lang.EN]: 'Get started',
-    [lang.ES]: 'Get started',
-    [lang.PT]: 'Get started'
+    [lang.ES]: 'Comenzar',
+    [lang.PT]: 'Comece agora'
   },
   privacyPolicy: {
     [lang.EN]: 'Privacy policy',
-    [lang.ES]: 'Privacy policy',
-    [lang.PT]: 'Privacy policy'
+    [lang.ES]: 'Política de privacidad',
+    [lang.PT]: 'Política de privacidade'
   },
   termsOfUse: {
-    [lang.EN]: 'Terms of use',
-    [lang.ES]: 'Terms of use',
-    [lang.PT]: 'Terms of use'
+    [lang.EN]: 'terms of use',
+    [lang.ES]: 'Términos de uso',
+    [lang.PT]: 'Termos de uso'
   },
   title: {
     [lang.EN]: 'Learn, memorize, master!',
-    [lang.ES]: 'Learn, memorize, master!',
-    [lang.PT]: 'Learn, memorize, master!'
+    [lang.ES]: '¡Aprende, memoriza, domina!',
+    [lang.PT]: 'Aprenda, memorize, domine!'
   }
 }
 
