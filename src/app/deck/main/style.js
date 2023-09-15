337a8d6be70b016ce73f281c51343f75adfbd4cb
deMGoncalves <cleber.engineer@hotmail.com>
2023-09-15 16:09:30 -0300

    fix: devo usaro o smallest para nao ter problema com erro de calculo

--- src/app/deck/main/style.js
+++ src/app/deck/main/style.js
@@ -7,7 +7,7 @@ const style = css`
     gap: var(--spacing_inset-lg);
     margin: 0 auto;
     max-width: 425px;
-    min-height: calc(100vh - 148px);
+    min-height: calc(100svh - 148px);
     padding: var(--spacing_inset-xs);
     width: 100%;
   }
