1e2f11d9201f6dfe1939d11ccdd288351c1c712b
deMGoncalves <cleber.engineer@hotmail.com>
2023-09-15 19:28:40 -0300

    chore(deck): area para acionar o processo de criacao de novos cartoes

--- src/app/deck/main/createCard/component.jsx
+++ src/app/deck/main/createCard/component.jsx
@@ -1,11 +1,20 @@
 import Button from '@elements/button'
 import h from '@standard/h'
 import redirectTo from './redirectTo'
+import stack from '@elements/stack'
+import style from './style'
+import text from '@elements/text'
 import translate from './translate'
 
 function component () {
   return (
-    <Button onClick={() => redirectTo.createCard()} wide>{translate.createCard}</Button>
+    <div className={style.createCard}>
+      <stack.Div column>
+        <text.Span master darker xs bold>{translate.title}</text.Span>
+        <text.Span master xxxs>{translate.description}</text.Span>
+      </stack.Div>
+      <Button onClick={() => redirectTo.createCard()} wide>{translate.createCard}</Button>
+    </div>
   )
 }
 
