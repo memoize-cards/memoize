d205475e94d2aacd66211d4f04f6abac2a0ec16b
deMGoncalves <cleber.engineer@hotmail.com>
2023-09-20 16:55:40 -0300

    style(app): padronizacao do codigo

--- src/app/front/front.js
+++ src/app/front/front.js
@@ -6,31 +6,30 @@ import result from '@standard/result'
 
 @paint(component)
 class Front {
-  #validity
-  #value
+  #data = {}
 
   get validity () {
-    return (this.#validity ??= '')
+    return (this.#data.validity ??= '')
   }
 
   get value () {
-    return (this.#value ??= '')
+    return (this.#data.value ??= '')
   }
 
   constructor (props) {
-    this.#value = props.value
+    Object.assign(this.#data, { ...props })
   }
 
   @filter.value
   @validator.required
   onChange (value) {
-    this.#value = value
+    Object.assign(this.#data, { value })
     return this
   }
 
   @repaint
-  [result.Ok] (state) {
-    this.#validity = state
+  [result.Ok] (validity) {
+    Object.assign(this.#data, { validity })
     return this
   }
 }
