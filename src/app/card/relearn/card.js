83c0cdf52a3b14080831ef9e3727f112034fd942
deMGoncalves <cleber.engineer@hotmail.com>
2023-09-25 15:20:24 -0300

    refactor(card): padronizando o adapter para atualizacao de dados

--- src/app/card/relearn/card.js
+++ src/app/card/relearn/card.js
@@ -2,65 +2,79 @@ import * as f from '@standard/f'
 import EasyFactor from './easyFactor'
 import Interval from './interval'
 import Lapse from './lapse'
+import request from '@standard/request'
 import storage from './storage'
 import type from './type'
 
 class Card {
-  #id
+  #data = {}
   #lapse
 
   get id () {
-    return this.#id
+    return this.#data.id
   }
 
   get type () {
     return 'Relearn'
   }
 
-  constructor (id, lapse) {
-    this.#id = id
+  constructor (data, lapse) {
+    Object.assign(this.#data, { ...data })
     this.#lapse = lapse
   }
 
   @storage.push
   again () {
     this.#lapse.inc1()
-    return {
+    Object.assign(this.#data, {
       interval: Interval.temMinutes,
       lapse: this.#lapse.value
-    }
+    })
+    return this
   }
 
   @storage.push
   easy () {
-    return {
+    Object.assign(this.#data, {
       easyFactor: EasyFactor.value,
       interval: Interval.fourDays,
       lapse: Lapse.zero,
       type: type.REVIEW
-    }
+    })
+    return this
   }
 
   @storage.push
   good () {
-    return {
+    Object.assign(this.#data, {
       easyFactor: EasyFactor.value,
       interval: Interval.oneDay,
       lapse: Lapse.zero,
       type: type.REVIEW
-    }
+    })
+    return this
   }
 
   @storage.push
   hard () {
-    return {
+    Object.assign(this.#data, {
       interval: Interval.twelveHours
+    })
+    return this
+  }
+
+  [request.Post] () {
+    return {
+      easyFactor: this.#data.easyFactor,
+      interval: this.#data.interval,
+      lapse: this.#data.lapse,
+      type: this.#data.type
     }
   }
 
   static create (data) {
     const lapse = Lapse.create(data.lapse)
-    return new Card(data.id, lapse)
+    return new Card(data, lapse)
   }
 
   static is (data) {
--- src/app/card/relearn/storage.js
+++ src/app/card/relearn/storage.js
@@ -1,10 +1,20 @@
 import interceptor from '@standard/interceptor'
+import request from '@standard/request'
+import result from '@standard/result'
 import supabase from '@artifact/supabase'
 
+async function update (card) {
+  const payload = await card[request.Post]?.()
+  const { data, error } = await supabase.from('card').update(payload).eq('id', card.id).select()
+  error
+    ? card[result.Error]?.(error)
+    : card[result.Ok]?.(data)
+}
+
 const push = interceptor(async function storage (args, next) {
-  const card = next(...args)
-  await supabase.from('card').update(card).eq('id', this.id)
-  return card
+  await next(...args)
+  await update(this)
+  return this
 })
 
 export default {
