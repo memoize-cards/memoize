c27099ad2a29c7c39f8f195cb69c85eff738458f
deMGoncalves <cleber.engineer@hotmail.com>
2023-10-05 20:24:21 -0300

    fix(language): erro de digitacao

--- src/site/master/footer/language/component.jsx
+++ src/site/master/footer/language/component.jsx
@@ -7,7 +7,7 @@ function component () {
   return (
     <button className={style.language}>
       <icon.Language />
-      <select className={style.language__select} onChange={(e) => (lang.value = e.target.valud)}>
+      <select className={style.language__select} onChange={(e) => (lang.value = e.target.value)}>
         <option className={style.language__option} value={lang.EN}>EN</option>
         <option className={style.language__option} value={lang.ES}>ES</option>
         <option className={style.language__option} value={lang.PT}>PT</option>
