9f56be19664e38eeca0e8bfc62f54fa7db69c41e
deMGoncalves <cleber.engineer@hotmail.com>
2023-10-05 18:51:19 -0300

    refactor(dashboard): eh responsabilidade de user buscar os dados

--- src/app/dashboard/header/user/component.jsx
+++ src/app/dashboard/header/user/component.jsx
@@ -1,14 +1,13 @@
-import cookie from '@standard/cookie'
 import h from '@standard/h'
 import style from './style'
 import text from '@elements/text'
 import translate from './translate'
 
-function component () {
+function component (user) {
   return (
     <div className={style.user}>
       <text.Span master xxxs>{translate.hello}</text.Span>
-      <text.Span master dark sm highlight bold>{cookie.name}</text.Span>
+      <text.Span master dark sm highlight bold>{user.name}</text.Span>
     </div>
   )
 }
