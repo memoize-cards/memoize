3c691fdd11cf012555a93539cb31ff8cfdfb92ed
deMGoncalves <cleber.engineer@hotmail.com>
2023-10-06 10:34:38 -0300

    refactor(app): usando o padrao de codificacao correto

--- src/app/createDeck/user.js
+++ src/app/createDeck/user.js
@@ -1,12 +1,14 @@
 import supabase from '@artifact/supabase'
 
 class User {
-  static get id () {
-    return (async () => {
-      const { data } = await supabase.auth.getUser()
-      return data.user.id
-    })()
-  }
+
 }
+
+Reflect.defineProperty(User, 'id', {
+  get: async function () {
+    const { data } = await supabase.auth.getUser()
+    return data.user.id
+  }
+})
 
 export default User
