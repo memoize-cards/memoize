eb52cefed515ba588f9b2ffd169d8d0feddeb464
deMGoncalves <cleber.engineer@hotmail.com>
2023-10-12 17:43:00 -0300

    refactor(card.feedback): usando o elementos certo e minimizando os styles

--- src/app/card/back/footer/style.js
+++ src/app/card/back/footer/style.js
@@ -1,31 +1,16 @@
 import css from '@standard/css'
 
 const style = css`
-  .footer {
-    display: flex;
-    flex-direction: column;
-    gap: var(--spacing_inset-xs);
-    margin: 0 auto;
-    width: 100%;
-  }
-
   .footer__feedback {
     display: grid;
     gap: var(--spacing_inset-nano);
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: repeat(2, 1fr);
+    width: 100%;
   }
 
   @media (width >= 769px) {
     .footer__feedback {
-      display: flex;
-    }
-
-    .footer__feedback button:nth-of-type(2) {
-      order: 1;
-    }
-
-    .footer__feedback button:nth-of-type(4) {
-      order: 2;
+      grid-template-columns: repeat(4, 1fr);
     }
   }
 `
