ba10472350a7f4d49466b0e7554230891924c1cc
deMGoncalves <cleber.engineer@hotmail.com>
2023-10-12 17:20:27 -0300

    fix(i18n): preciso fixar o EN como padrao

--- src/.prelude/standard/i18n/i18n.js
+++ src/.prelude/standard/i18n/i18n.js
@@ -2,7 +2,7 @@ import lang from '@standard/lang'
 
 function i18n (descriptor) {
   return new Proxy({}, {
-    get: (_, key) => descriptor[key][lang.value]
+    get: (_, key) => (descriptor[key][lang.value] ?? descriptor[key][lang.EN])
   })
 }
 
