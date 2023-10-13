9b2e2cdc4c7797e487121d8f987e2483e98ec12b
deMGoncalves <cleber.engineer@hotmail.com>
2023-10-13 12:31:06 -0300

    refactor(app.editcard): devemos manter os links no redirectto

--- src/app/editCard/redirectTo.js
+++ src/app/editCard/redirectTo.js
@@ -1,10 +1,15 @@
-import { urlFor } from '@standard/router'
+import { params, urlFor } from '@standard/router'
 import Deck from './deck'
 
 const redirectTo = {
   card () {
     location.assign(urlFor('card', { id: Deck.id }))
-    return redirectTo
+    return this
+  },
+
+  deleteCard () {
+    location.assign(urlFor('deleteCard', { deck: Deck.id, id: params.id }))
+    return this
   }
 }
 
