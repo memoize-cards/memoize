8e77d5a3def250ee3a1db3ef27a54befd1884b51
deMGoncalves <cleber.engineer@hotmail.com>
2023-10-13 10:57:26 -0300

    refactor(app.deletedeck): devemos usar o response para com o storage

--- src/app/deleteDeck/storage.js
+++ src/app/deleteDeck/storage.js
@@ -1,14 +1,14 @@
 import middleware from '@standard/middleware'
 import request from '@standard/request'
-import result from '@standard/result'
+import response from '@standard/response'
 import supabase from '@artifact/supabase'
 
 async function del (deck) {
   const payload = deck[request.Delete]?.()
   const { error } = await supabase.from('deck').delete().eq('id', payload.id)
   error
-    ? deck[result.Error]?.(error)
-    : deck[result.Ok]?.()
+    ? deck[response.Error]?.(error)
+    : deck[response.Ok]?.()
 }
 
 const push = middleware(function (deck) {
