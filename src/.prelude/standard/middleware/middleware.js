669dc27da58e6d7977943553031debbcf5798f71
deMGoncalves <cleber.engineer@hotmail.com>
2023-10-26 14:54:04 -0300

    refactor: neste caso o uso da funcao eh intencional

--- src/.prelude/standard/middleware/middleware.js
+++ src/.prelude/standard/middleware/middleware.js
@@ -1,7 +1,7 @@
 function middleware (functionRef) {
   return function (ClassRef) {
     return new Proxy(
-      (...args) => {
+      function (...args) {
         const instance = new ClassRef(...args)
         setImmediate(() => functionRef(instance))
         return instance
