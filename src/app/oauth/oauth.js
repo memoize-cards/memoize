a8f1d98d493412afd88becf0b11f88e986e27769
deMGoncalves <cleber.engineer@hotmail.com>
2023-10-26 14:57:02 -0300

    refactor: devemos usar arrow functions para callback

--- src/app/oauth/oauth.js
+++ src/app/oauth/oauth.js
@@ -2,7 +2,7 @@ import middleware from '@standard/middleware'
 import redirectTo from './redirectTo'
 import supabase from '@artifact/supabase'
 
-const required = middleware(async function () {
+const required = middleware(async () => {
   const { data } = await supabase.auth.getUser()
   !data.user && redirectTo.logOut()
 })
