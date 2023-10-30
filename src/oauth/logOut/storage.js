aceb83982c19220b0577fc66e44a5aaaca1dc9e7
deMGoncalves <cleber.engineer@hotmail.com>
2023-10-30 07:52:13 -0300

    chore(logout): funcoes callback deve ser arrow functions

--- src/oauth/logOut/storage.js
+++ src/oauth/logOut/storage.js
@@ -2,7 +2,7 @@ import middleware from '@standard/middleware'
 import response from '@standard/response'
 import supabase from '@artifact/supabase'
 
-const push = middleware(async function (auth) {
+const push = middleware(async (auth) => {
   await supabase.auth.signOut()
   auth[response.Ok]?.()
 })
