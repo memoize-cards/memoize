d3167084c51721813fe57e159f276bff63686d87
deMGoncalves <cleber.engineer@hotmail.com>
2024-01-02 19:02:23 -0300

    refactor: nao preciso mais de forcar a formatacao

--- src/app/card/front/main/front.jsx
+++ src/app/card/front/main/front.jsx
@@ -2,13 +2,7 @@ import global from '@standard/global'
 import h from '@standard/h'
 
 function component () {
-  /**
-   * TODO: Depois que padronizar a contrucao do front, variavel
-   * heading sera removido
-   */
-  const content = global.front?.includes('#')
-    ? global.front
-    : `#${global.front ?? ''}`
+  const content = (global.front ?? '')
 
   return (
     <m-markdown>
@@ -18,3 +12,5 @@ function component () {
 }
 
 export default component
+
+
