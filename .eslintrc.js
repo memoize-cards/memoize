79d265d813159468e351b7850df6d692dbb0fb0e
deMGoncalves <cleber.engineer@hotmail.com>
2024-02-17 19:59:52 -0300

    chore(console.log): nao permite o uso do console

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,7 @@ module.exports = {
     ],
     '@typescript-eslint/no-var-requires': 'off',
     'no-async-promise-executor': 'off',
+    'no-console': 'error',
     'no-return-assign': 'off',
     'no-sequences': 'off',
     'no-unused-expressions': 'off'
