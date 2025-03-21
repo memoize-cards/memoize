2c716cb2f37f8a87b856b3c79a6afab67779b7ff
deMGoncalves <cleber.engineer@hotmail.com>
2025-03-21 17:08:06 -0300

    fix: erro de nomenclatura

--- src/oauth/passwordReseted/index.js
+++ src/oauth/passwordReseted/index.js
@@ -3,5 +3,5 @@ import router from "standard/router";
 
 router("/oauth/password-reseted", function passwordReseted() {
   import("./oauth");
-  renderer("<m-email-password-reseted></m--password-reseted>");
+  renderer("<m-password-reseted></m--password-reseted>");
 });
