31b528003e6638146d5af8f35a7f90792f56203c
deMGoncalves <cleber.engineer@hotmail.com>
2025-03-31 16:03:02 -0300

    feat: limpa o cache para quando o usuario fizer o logout

---
+++ packages/standard/runLock/clear.js
@@ -0,0 +1,7 @@
+import cache from "./cache";
+
+function clear() {
+  cache.clear();
+}
+
+export default clear;
--- packages/standard/runLock/index.js
+++ packages/standard/runLock/index.js
@@ -1,2 +1,3 @@
 export { default } from "./runLock";
+export { default as clear } from "./clear";
 export { default as ttl } from "./ttl";
