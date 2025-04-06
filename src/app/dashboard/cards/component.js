5e79eeb161d73ea728c513a4aee9735fa917bd50
cleitoncarloss <cleitoncarlos.programmer@gmail.com>
2025-04-06 10:53:28 -0300

    style: altera a escala do tamanho

--- src/app/dashboard/cards/component.js
+++ src/app/dashboard/cards/component.js
@@ -3,7 +3,7 @@ import { urlFor } from "standard/router";
 
 function component() {
   return html`
-    <m-text color="primary-darker" weight="bold" size="sm" family="highlight">Meus cartões</m-text>
+    <m-text color="primary-darker" weight="bold" size="xs" family="highlight">Meus cartões</m-text>
   `;
 }
 
